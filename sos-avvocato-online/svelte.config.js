87d657f647a6e27db9811e7acb83b81cc3c98a30
Andrea <a.mussari@studenti.unipi.it>
2025-06-26 17:05:27 +0200

    .

--- sos-avvocato-online/svelte.config.js
+++ sos-avvocato-online/svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: { adapter: adapter() },
-	paths: { base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "" }
+	paths: { base: '/SOS_Avvocato_Online' }
 };
 
 export default config;
