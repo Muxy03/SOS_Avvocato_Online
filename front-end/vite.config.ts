bdd0a05252a2dbc649efd3ec84e44e3ef38fa8f8
Andrea <a.mussari@studenti.unipi.it>
2025-07-16 14:10:38 +0200

    redefine deploy

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -5,5 +5,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
-	base: '/SOS_Avvocato_Online/front-end/' // Your repository name
 });
