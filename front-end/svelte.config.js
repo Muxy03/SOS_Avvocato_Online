e4f22056c405c302284dfb2b7bde65a12814aabb
Andrea <a.mussari@studenti.unipi.it>
2025-07-16 03:58:10 +0200

    .

--- front-end/svelte.config.js
+++ front-end/svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter(), base: process.env.NODE_ENV === 'production' ? '/SOS_Avvocato_Online' : '' }
+	kit: { adapter: adapter()}
 };
 
 export default config;
